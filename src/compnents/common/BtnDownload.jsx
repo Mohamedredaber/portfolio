e9959cc8a5b51112cb114ea83a597b6e2daf0350
mohamed reda <ridabarhouma@gmail.com>
2026-01-30 04:59:05 +0100

    test deloy

--- src/compnents/common/BtnDownload.jsx
+++ src/compnents/common/BtnDownload.jsx
@@ -2,15 +2,16 @@ import downloadcvbtndata from "../../data/downloadcvbtndata";
 import { useSelector } from "react-redux";
 import { useMemo } from "react";
 import "./BtnDownload.css";
-function BtnDownload() {
 
+function BtnDownload() {
+  const base = import.meta.env.BASE_URL;
   const lang = useSelector((state) => state.language.langue) || "en";
   const t = useMemo(
     () => downloadcvbtndata[lang] || downloadcvbtndata.en,
     [lang],
   );
   return (
-    <a className="btn ghost" href="/images/cv/cv.pdf" download>
+    <a className="btn ghost" href={`${base}images/cv/cv.pdf`} download>
       <span className="dl">⬇</span> {t.btnCV}
     </a>
   );
