52d67c54ca6dc24e33955eaf4cd3cb087b81ff7b
mohamed reda <ridabarhouma@gmail.com>
2026-03-17 15:23:12 +0100

    g

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./app/store.jsx";
 import { HashRouter } from "react-router-dom";
-import './index.css'
+import { store } from "./app/store.jsx";
+import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
-      <Provider store ={store}>
+    <HashRouter basename="/portfolio/">
+      <Provider store={store}>
         <App />
       </Provider>
     </HashRouter>
