2768a61f531e6b5821c81d481c00a50ef7c8c05f
mohamed reda <ridabarhouma@gmail.com>
2026-03-17 17:29:03 +0100

    Fix: Change HashRouter to BrowserRouter for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store.jsx";
 import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/portfolio/">
       <Provider store={store}>
         <App />
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 );
